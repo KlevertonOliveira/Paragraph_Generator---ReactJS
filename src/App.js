353f36aeedd49a016af41c69a2371a3e68ba4e4f
KlevertonOliveira <kleverton.oliveira.al@gmail.com>
2021-03-02 18:21:46 -0300

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import  React, {useState} from 'react';
+import data from "./data";
 
 function App() {
+
+  const [numberOfParagraphs, setNumberOfParagraphs] = useState(0);
+  const [paragraphs, setParagraphs] = useState([]);
+
+  const handleInputChange = (e) =>{
+    let value = e.target.value;
+    setNumberOfParagraphs(value);
+  }
+
+  const generateParagraphs = (e) => {
+    e.preventDefault();
+    setParagraphs(
+      data.filter(
+        (item, index)=>(index + 1) <= parseInt(numberOfParagraphs)
+      )
+    )
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <h1>Tired of Lorem Ipsum?</h1>
+      <form action="" onSubmit={generateParagraphs}>
+        <div className="form-control">
+          <div>
+            <label htmlFor="paragraphs">Paragraphs : </label>
+            <input 
+              type="number"
+              id="paragraphs"
+              name="paragraphs"
+              min="0"
+              max="9"
+              value={numberOfParagraphs}
+              onChange = {handleInputChange}
+              />
+            </div>
+            <button type="submit">Generate</button>
+          </div>
+
+        <div className="paragraphs">
+          {
+            paragraphs.map((paragraph)=>{
+              return <p>{paragraph}</p>
+            })
+          }
+        </div>
+      </form>
+    </main>
   );
 }
 
